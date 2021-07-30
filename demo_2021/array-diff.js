e8a0556fd6b460a4bf1db5ed88f787f74a68e89d
gyh <mypridelife@163.com>
2021-07-30 03:31:36 +0000

    .

--- demo_2021/array-diff.js
+++ demo_2021/array-diff.js
@@ -1,5 +1,5 @@
-ES6中使用Set结构:let a = new Set([1, 2, 3]);
-let b = new Set([3, 5, 2]); 
+ES6中使用Set结构: let a = new Set([1, 2, 3]);
+let b = new Set([3, 5, 2]);
 
 // 并集
 let unionSet = new Set([...a, ...b]);
@@ -12,5 +12,6 @@ let intersectionSet = new Set([...a].filter(x => b.has(x)));
 // ab差集
 let differenceABSet = new Set([...a].filter(x => !b.has(x)));
 // [1]
-再把Set转换为数组即可.let arr = Array.from(set);
+// 再把Set转换为数组即可
+let arr = Array.from(set);
 // 或 let arr = [...set];
